79cb219dd265358036985465fcc1ec94cdae7aa7
andrew <andreyhorolsky@gmail.com>
2023-02-28 21:26:09 +0300

    API Declaration 5.2 - search animals is done

--- animals.php
+++ animals.php
@@ -1,10 +1,28 @@
 <?php
 
+//поиск типов животного и добавление информации в результат
+function getAnimalsType($connect, $animal){
+    //берется id животного
+    $id = $animal["id"];
+    $typesList = [];
+
+    //запрос в бд о его типах
+    $animal_types = mysqli_query($connect, "SELECT `id_type` FROM `animal_types` WHERE `id_animal` = '$id'");
+    
+    //добавление в массив
+    while ($type = mysqli_fetch_assoc($animal_types)){
+        array_push($typesList, array_values($type));
+    }
+    $animal_types = ["animalTypes" => $typesList];
+
+    return array_merge(array_slice($animal, 0, 1), $animal_types, array_slice($animal, 1));
+}
+
+
 function getAnimalById($connect, $id){
     //запрос в бд
     $animal = mysqli_query($connect, "SELECT `id`, `weight`, `length`, `height`, `gender`, `lifeStatus`, `chippingDateTime`, `chipperId`, `chippingLocationId`, `deathDateTime` FROM `animals` WHERE `id` = '$id'");
-    $animal_types = mysqli_query($connect, "SELECT `id_type` FROM `animal_types` WHERE `id_animal` = '$id'");
-
+    
     //неверный id - 400
     if ($id <= 0 || $id == null){
         http_response_code(400);
@@ -30,23 +48,82 @@ function getAnimalById($connect, $id){
     }
 
     $animal = mysqli_fetch_assoc($animal);
-    $typesList = [];
 
-    while ($type = mysqli_fetch_assoc($animal_types)){
-        array_push($typesList, array_values($type));
-    }
-
-    $animal_types = [];
-    for ($i = 0; $i < count($typesList); $i++){
-        $animal_types[] = $typesList[$i];
-    }
-
-    $animal_types = ["animalTypes" => $typesList];
-    $res = array_merge(array_slice($animal, 0, 1), $animal_types, array_slice($animal, 1));
-    
-    echo json_encode($res);
+    echo json_encode(getAnimalsType($connect, $animal));
 }
 
-function getSearchAnimals($connect){
 
+function getSearchAnimals($connect){
+    
+    //запрос и кол-во параметров
+    $query = "SELECT `id`, `weight`, `length`, `height`, `gender`, `lifeStatus`, `chippingDateTime`, `chipperId`, `chippingLocationId`, `deathDateTime` FROM `animals` WHERE ";
+    $first = false;
+
+    //переменные по дефолту
+    $startDateTime = $_GET['startDateTime'] ?? null;
+    $endDateTime = $_GET['endDateTime'] ?? null;
+    $chipperId = $_GET['chipperId'] ?? null;
+    $chippingLocationId = $_GET['chippingLocationId'] ?? null;
+    $lifeStatus = $_GET['lifeStatus'] ?? null;
+    $gender = $_GET['gender'] ?? null;
+    $from = $_GET['from'] ?? 0;
+    $size = $_GET['size'] ?? 10;
+
+    //from < 0 || size <= 0 - 400
+    if ($from < 0 || $size <= 0){
+        http_response_code(400);
+        echo json_encode([
+            "status" => false,
+            "message" => "Bad request"
+        ]);
+        return;
+    }
+
+    //Неверные авторизационные данные - 401  ???????
+
+
+    //создание запроса в зависимости от реквеста
+    if (isset($startDateTime)){
+        $query .= "`chippingDateTime` >= '$startDateTime' ";
+        $first = true;
+    }
+    if (isset($endDateTime)){
+        if ($first) $query .= " AND ";
+        else $first = true;
+        $query .= "`chippingDateTime` <= '$endDateTime' ";
+    }
+    if (isset($chipperId)){
+        if ($first) $query .= " AND ";
+        else $first = true;
+        $query .= "`chipperId` = '$chipperId' ";
+    }
+    if (isset($chippingLocationId)){
+        if ($first) $query .= " AND ";
+        else $first = true;
+        $query .= "`chippingLocationId` = '$chippingLocationId' ";
+    }
+    if (isset($lifeStatus)){
+        if ($first) $query .= " AND ";
+        else $first = true;
+        $query .= "`lifeStatus` = '$lifeStatus' ";
+    }
+    if (isset($gender)){
+        if ($first) $query .= " AND ";
+        else $first = true;
+        $query .= "`gender` = '$gender' ";
+    }
+
+    $animalList = [];
+    $searchAnimals = mysqli_query($connect, $query);
+
+    //пропускаем указанное кол-во элементов
+    for($i=0; $i++<$from; mysqli_fetch_assoc($searchAnimals));
+    
+    //вывод указаного кол-ва элементов
+    while ($animal = mysqli_fetch_assoc($searchAnimals)){
+        $animalList[] = getAnimalsType($connect, $animal);
+        if (--$size === 0) break;
+    }
+
+    echo json_encode($animalList);
 }
