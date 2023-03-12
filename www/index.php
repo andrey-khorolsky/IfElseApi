47c94f8090db326f4093caaa39c78f1f1d1c82f0
andrew <andreyhorolsky@gmail.com>
2023-03-12 21:28:45 +0300

    trying to route...

--- www/index.php
+++ www/index.php
@@ -13,11 +13,11 @@ header("Content-type: application/json");
 
 $method = $_SERVER["REQUEST_METHOD"];
 $q = $_GET["q"];
-$params = explode("/", $q);
-$page = [];
-for ($i = 0; $i < count($params); $i++){
-    $page[] = $params[$i];
-}
+$page = explode("/", $q);
+// $page = [];
+// for ($i = 0; $i < count($params); $i++){
+//     $page[] = $params[$i];
+// }
 
 
 if ($page[0] === "accounts"){
@@ -33,12 +33,19 @@ if ($page[0] === "accounts"){
 } elseif ($page[0] === "animals") {
     if (isset($page[2]) && $page[2] === "locations")
         getVisitedLocations($connect, $page[1]);
-    elseif ($page[1] === "types")
-        getAnimalTypeById($connect, $page[2]);
-    elseif ($page[1] === "search")
+    elseif (isset($page[1]) && $page[1] === "types"){
+        if ($method === "GET")
+            getAnimalTypeById($connect, $page[2]);
+        elseif ($method === "POST")
+            addAnimalType($connect);
+        }
+    elseif (isset($page[1]) && $page[1] === "search")
         getSearchAnimals($connect);
     else
-        getAnimalById($connect, $page[1]);
+        if ($method === "GET")
+            getAnimalById($connect, $page[1]);
+        elseif ($method === "POST")
+            addAnimal($connect);
 } elseif ($page[0] === "locations"){
     if ($method === "DELETE")
         deleteLocationById($connect, $page[1]);
@@ -51,3 +58,13 @@ if ($page[0] === "accounts"){
 } elseif ($page[0] === "registration") {
     registrationAccount($connect);
 }
+
+// switch ($page[0]) {
+//     case 'registration':
+//         require_once("authentication.php");
+//         break;
+    
+//     default:
+//         # code...
+//         break;
+// }
