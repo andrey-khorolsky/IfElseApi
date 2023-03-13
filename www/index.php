2361e00659332e2b420fe67509ca27bb2f6f7110
andrew <andreyhorolsky@gmail.com>
2023-03-13 19:35:07 +0300

    added some functions

--- www/index.php
+++ www/index.php
@@ -27,8 +27,12 @@ if ($page[0] === "accounts"){
         elseif ($method === "PUT")
             updateAccount($connect, $page[1]);
 } elseif ($page[0] === "animals") {
-    if (isset($page[2]) && $page[2] === "locations")
-        getVisitedLocations($connect, $page[1]);
+    if (isset($page[2]) && $page[2] === "locations"){
+        if ($method === "GET")
+            getVisitedLocations($connect, $page[1]);
+        elseif ($method === "POST")
+            addVisitedLocationToAnimal($connect, $page[1], $page[3]);
+    }
     elseif (isset($page[1]) && $page[1] === "types"){
         if ($method === "GET")
             getAnimalTypeById($connect, $page[2]);
@@ -42,6 +46,8 @@ if ($page[0] === "accounts"){
             getAnimalById($connect, $page[1]);
         elseif ($method === "POST")
             addAnimal($connect);
+        elseif ($method === "PUT")
+            updateAnimal($connect, $page[1]);
 } elseif ($page[0] === "locations"){
     if ($method === "DELETE")
         deleteLocationById($connect, $page[1]);
@@ -54,11 +60,14 @@ if ($page[0] === "accounts"){
 } elseif ($page[0] === "registration") {
     registrationAccount($connect);
 }
-
+// require_once($page[0]."_route.php");
 // switch ($page[0]) {
 //     case 'registration':
-//         require_once("authentication.php");
+//         require_once("authentication_route.php");
 //         break;
+//     case 'accounts':
+    //     require_once("authentication_route.php");
+    //     break;
     
 //     default:
 //         # code...
